932d485d96ffe7e4864ac68b8132db20b69fdf88
JUANESZz1 <159485717+JUANESZz1@users.noreply.github.com>
2024-02-27 10:37:29 -0500

    Update TV.java

--- src/main/televisores/TV.java
+++ src/main/televisores/TV.java
@@ -5,15 +5,15 @@ public class TV {
 	private int precio = 500;
 	boolean estado;
 	private int volumen =1;
-	
+	private int numTV = 0;
 	 
 	public TV(Marca marca, boolean estado) {
-		
-		
-		
 		System.out.println("Se ha creado un objeto de clase tv");
-		;
-		System.out.println("Hay " + getNumero());
+		
+		    numTV = ++numTV;
+			System.out.println("Se ha construido un nuevo objeto");
+			System.out.println("Hay " + numTV + " objetos");
+		}
 		
 	}
 	
@@ -38,4 +38,27 @@ public void setEstado(boolean e) {
 	this.estado = e;
 }
 
+public void setNumeroTV(int numero){
+	this.numero = numeroTV;
+
+} 
+public int getNumeroTV(){
+	return this.numeroTV;
+}
+
+public void turnOn(){
+if (estado == false){
+estado = true;
+System.out.println("El tv se encendio correctamente");
+}else{
+	system.out.println("El TV ya estaba encendido");
+}
+public void turnOff(){
+	if estado == true{
+		estado == false
+		System.out.println("El tv se apago correctamente");
+	}else{
+		System.out.println("El tv ya estaba apagado");
+	}
+}
 }
